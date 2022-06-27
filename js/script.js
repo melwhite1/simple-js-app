d3426d0996c90825e97ba783c36daae6e4746f56
melwhite1 <melissawhite668@gmail.com>
2022-06-27 14:49:16 -0400

    Created pokemonList

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood = 'chocolate chip cookies';
-document.write(favoriteFood)
+let pokemonList = [
+  {name: 'Pikachu', height: 1.04, type: 'electric'},
+  {name: 'Charizard', height:1.7, type: ['fire', 'flying']},
+  {name: 'Squirtle', height:.5, type:'water'}
+];
+document.write(pokemonList);
