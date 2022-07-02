fe112c2a0464803e603112b2654eeb7551b51c03
melwhite1 <melissawhite668@gmail.com>
2022-07-02 13:12:49 -0400

    Create for loop for name and height

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ let pokemonList = [
   {name: 'Charizard', height:1.7, type: ['fire', 'flying']},
   {name: 'Squirtle', height:.5, type:'water'}
 ];
-document.write(pokemonList);
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if(pokemonList[i].height <1){
+  document.write("Name: " + pokemonList[i].name + '<br>' + " Height: " + pokemonList[i].height + " - What a tiny, adorable Pokemon! " + '<br>');}
+  else if(pokemonList[i].height >1){
+  document.write("Name: " + pokemonList[i].name + '<br>' + " Height: " + pokemonList[i].height + '<br>' + '<p>')
+  }
+}
